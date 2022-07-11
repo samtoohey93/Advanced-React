6f7477b8d885f2a215710ade500986a8ea346f0d
Sam Toohey <hello@samtoohey.xyz>
2022-07-11 21:36:11 +1000

    page creation

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+const indexPage = () => {
+  return <div>Howdy</div>;
 };
 
-export default index;
+export default indexPage;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const sellPage = () => {
+  return <div>sellPage</div>;
+};
+
+export default sellPage;
