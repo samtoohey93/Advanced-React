3c46ba7a88982f74b4b8c1bb8f6d92465990209b
Sam Toohey <hello@samtoohey.xyz>
2022-09-15 08:01:07 +1000

    keystone line changes

--- sick-fits/backend/schemas/Product.ts
+++ sick-fits/backend/schemas/Product.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { relationship, select, text } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
 import "dotenv/config";
@@ -36,42 +35,3 @@ export const Product = list({
     }),
   },
 });
-=======
-import { relationship, select, text } from "@keystone-next/fields";
-import { list } from "@keystone-next/keystone/schema";
-import "dotenv/config";
-
-export const Product = list({
-  //Todo
-  //access:
-  fields: {
-    name: text({ isRequired: true }),
-    description: text({
-      ui: {
-        displayMode: "textarea",
-      },
-    }),
-    photo: relationship({
-      ref: "ProductImage.product",
-      ui: {
-        displayMode: "cards",
-        cardFields: ["image", "altText"],
-        inLineCreate: { fields: ["image", "altText"] },
-        inlineEdit: { fields: ["image", "altText"] },
-      },
-    }),
-
-    status: select({
-      options: [
-        { label: "Draft", value: "Draft" },
-        { label: "Available", value: "Available" },
-        { label: "Unavailable", value: "Unavailable" },
-      ],
-      defaultValue: "Draft",
-      ui: {
-        displayMode: "segmented-control",
-      },
-    }),
-  },
-});
->>>>>>> cb80dae16e7a8c280e86e3ac4d3e7afd16de5632
--- sick-fits/backend/schemas/User.ts
+++ sick-fits/backend/schemas/User.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { text, password, relationship } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
 import "dotenv/config";
@@ -13,19 +12,3 @@ export const User = list({
     //Todo add roles, cart and orders
   },
 });
-=======
-import { text, password, relationship } from "@keystone-next/fields";
-import { list } from "@keystone-next/keystone/schema";
-import "dotenv/config";
-
-export const User = list({
-  //access:
-  //UI
-  fields: {
-    name: text({ isRequired: true }),
-    email: text({ isRequired: true, isUnique: true }),
-    password: password(),
-    //Todo add roles, cart and orders
-  },
-});
->>>>>>> cb80dae16e7a8c280e86e3ac4d3e7afd16de5632
